065348a8b77d4bb3bd86ec165f6950e592ff2334
Bambo <bambo@prayerbox.co>
2020-02-27 00:27:04 +0100

    L.C.m

--- math_example.py
+++ math_example.py
@@ -95,3 +95,25 @@ print('x // y =',x//y)
 
 # Output: x ** y = 50625
 print('x ** y =',x**y)
+
+## L.C.M of two numbers
+def compute_lcm(x, y):
+
+   # choose the greater number
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+
+num1 = 54
+num2 = 24
+
+print("The L.C.M. of 54 and 24 is", compute_lcm(num1, num2))
