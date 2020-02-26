e85154f0702ab9b4f8f2941f9bf6ed85159aa6ec
Bambo <bambo@prayerbox.co>
2020-02-27 00:19:25 +0100

    Arithmetic operators

--- math_example.py
+++ math_example.py
@@ -74,3 +74,24 @@ else:
        factorial = factorial*i
    print("The factorial of",num,"is",factorial)
 
+## Arithmetic Operations 
+x = 15
+y = 4
+
+# Output: x + y = 19
+print('x + y =',x+y)
+
+# Output: x - y = 11
+print('x - y =',x-y)
+
+# Output: x * y = 60
+print('x * y =',x*y)
+
+# Output: x / y = 3.75
+print('x / y =',x/y)
+
+# Output: x // y = 3
+print('x // y =',x//y)
+
+# Output: x ** y = 50625
+print('x ** y =',x**y)
