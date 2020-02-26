cbfbdf5ed6baf744022e5ae48b9bd30429ea193f
Bambo <bambo@prayerbox.co>
2020-02-26 23:53:50 +0100

    Calendar and math examples

---
+++ calendar_app.py
@@ -0,0 +1,20 @@
+# Program to display calendar of the given month and year
+
+# importing calendar module
+import calendar
+from datetime import date, datetime
+
+mydate = datetime.now()
+year_ = mydate.strftime("%Y")
+month_ = mydate.strftime("%B")
+
+
+
+
+# To take month and year input from the user comment the block above and uncomment the one below
+# year_ = int(input("Enter year: "))
+# month_ = int(input("Enter month: "))
+
+# display the calendar
+print('Calendar for ' + str(month_) + ' ' + str(year_))
+# print(calendar.month(year_, month_))
---
+++ math_example.py
@@ -0,0 +1,76 @@
+import math
+
+## square root of 4
+squareroot_of_2 = math.sqrt(4)
+print('The square root of 4 is ' + str(squareroot_of_2))
+
+## calculate prime numbers
+# prime numbers are greater than 1
+
+num = 407
+if num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+       
+# if input number is less than
+# or equal to 1, it is not prime
+else:
+   print(num,"is not a prime number")
+   
+## Take a list of numbers to find if they are divisible by 13
+my_list = [12, 65, 54, 39, 102, 339, 221,]
+
+# use anonymous function to filter
+result = list(filter(lambda x: (x % 13 == 0), my_list))
+
+# display the result
+print("Numbers divisible by 13 are",result)
+
+
+# Find the largest number among the three input numbers
+# change the values of num1, num2 and num3
+# for a different result
+num1 = 10
+num2 = 14
+num3 = 12
+
+# uncomment following lines to take three numbers from user
+#num1 = float(input("Enter first number: "))
+#num2 = float(input("Enter second number: "))
+#num3 = float(input("Enter third number: "))
+
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+
+print("The largest number is", largest)
+
+# Find the factorial of a number provided by the user.
+
+# change the value for a different result
+num = 7
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
+
