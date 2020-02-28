aee825828b272cc3ff41c03b534d139a4c81686a
Bambo <bambo@prayerbox.co>
2020-02-28 03:08:50 +0100

    Added string join and interactive calculator

---
+++ string_join.py
@@ -0,0 +1,10 @@
+# initialize the list of string
+str_list = ['I', 'love', 'coding', 'in', 'Python']
+# initialize the separator
+separator = ' '
+
+# use python string joint to join the words of the string
+output = separator.join(str_list)
+
+# print the output result
+print(output)
