47449f039b98ab36042a57d6f94cfc996d55a195
Bambo <bambo@prayerbox.co>
2020-03-01 15:19:08 +0100

    Fortune game

---
+++ fortunegame.py
@@ -0,0 +1,14 @@
+import random
+
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It is certain'
+    elif answerNumber == 2:
+        return 'It is decidedly so'
+    elif answerNumber == 3:
+        return 'Yes'
+    
+    
+r = random.randint(1, 3)
+fortune = getAnswer(r)
+print(fortune)
