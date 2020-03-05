9b00e5a6d2f88a1c44755010587e018929e42941
Bambo <bambo@prayerbox.co>
2020-03-05 09:55:46 +0100

    Tiny changes

--- array.py
+++ array.py
@@ -1,7 +1,10 @@
-arr = [1,2,3,4,5,6,7]
+arr = [1,2,3,4,5,6,7] # define array
 
-arr.insert(3,10)
+arr.insert(3,10) # insert 10 after 3
 print(arr)
 
-arr.pop(3)
+arr.pop(3) # remove 10
+print(arr)
+
+arr.remove(3) # remove 3
 print(arr)
